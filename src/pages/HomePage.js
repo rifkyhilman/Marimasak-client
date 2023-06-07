50270fac7692a75cb7ed1e6231addf84bfe11320
rifkyhilman <rifkyhilman265@gmail.com>
2023-06-07 18:07:46 +0700

    :memo: Component Hero
    Component Hero

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Hero from '../Components/Hero';
+
 
 const HomePage = () => {
   return (
     <>
+    <Hero />
       <div className='container'> 
         <h1>Resep Masakan</h1>
         <p>Inilah tempatnya segala resep masakan enak! MAHI 
