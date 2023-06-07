4a915a450d475f32435b7453193e9c4d4f9a2e65
rifkyhilman <rifkyhilman265@gmail.com>
2023-06-07 23:35:08 +0700

    :busts_in_silhouette: Detail Resep Page
    Detail Resep Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import HomePage from './pages/HomePage';
+import DetailResepPage from './pages/DetailResepPage';
+import TentangPage from './pages/TentangPage';
 import Footer from './Components/Footer';
 import './Styles/index.scss';
 import './Styles/Responsive.scss';
@@ -13,10 +15,8 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        {/* <Route path='/detail/:id' element={<DetailResepPage />} />
-        <Route path='/kategori/:daerah' element={<KategoriDaerahPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} /> */}
+        <Route path='/tentang' element={<TentangPage />} />
+        <Route path='/detail/:id' element={<DetailResepPage />} />
       </Routes>
       <Footer />
     </Router>
