b71498d05509832a4ad41e02ce0ef98a2e9c18a7
indaahw <indahwarohmah21@yahoo.com>
2023-06-07 22:40:46 +0700

    TentangKami Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import HomePage from './pages/HomePage';
+import TentangPage from './pages/TentangPage';
 import Footer from './Components/Footer';
 import './Styles/index.scss';
 import './Styles/Responsive.scss';
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/tentang' element={<TentangPage />} />
         {/* <Route path='/detail/:id' element={<DetailResepPage />} />
         <Route path='/kategori/:daerah' element={<KategoriDaerahPage />} />
         <Route path='/login' element={<LoginPage />} />
