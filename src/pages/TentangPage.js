118c7aca41c47303d90af72bc314acbbd61b240d
indaahw <indahwarohmah21@yahoo.com>
2023-06-08 12:07:40 +0700

    mari masak merupakan

--- src/pages/TentangPage.js
+++ src/pages/TentangPage.js
@@ -6,7 +6,9 @@ const TentangPage = () => {
         <>
         <div className="container"> 
         <h1> MariMasak</h1>
-        <p>asdfghjklwertyudddddddfdffiop</p>
+        <p>MariMasak adalah website yang menyajikan resep makanan khas dari pulau Sumatera. </p>
+        <p>Tujuannya adalah memberikan kemudahan bagi pengguna dalam mendapatkan bahan makanan saat mereka tidak punya waktu untuk berbelanja di pasar.</p>
+        <p>MariMasak tidak hanya membantu mencari dan membeli bahan makanan, tetapi juga menyediakan resep dan petunjuk memasak untuk menginspirasi pengguna.</p>
         </div>
         </>
     )
