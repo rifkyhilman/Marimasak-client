be32cee646b0faab3132c8138f334ed453334088
indaahw <indahwarohmah21@yahoo.com>
2023-06-12 11:45:30 +0700

    style scss

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -4,19 +4,19 @@ import '../Styles/AboutUsPage.scss';
 const AboutUsPage = () => {
     return (
         <>
-        <div className="container"> 
-        <h1> MariMasak</h1>
-        <p>MariMasak adalah website yang menyajikan resep makanan khas dari pulau Sumatera. </p>
-        <p>Tujuannya adalah memberikan kemudahan bagi pengguna dalam mendapatkan bahan makanan saat mereka tidak punya waktu untuk berbelanja di pasar.</p>
-        <p>MariMasak tidak hanya membantu mencari dan membeli bahan makanan, tetapi juga menyediakan resep dan petunjuk memasak untuk menginspirasi pengguna.</p>
-        </div>
+            <div class="about-section">
+                <h1>MariMasak</h1>
+                <p>MariMasak adalah website yang menyajikan resep makanan khas dari pulau Sumatera. </p>
+                <p>Tujuannya adalah memberikan kemudahan bagi pengguna dalam mendapatkan bahan makanan saat mereka tidak punya waktu untuk berbelanja di pasar.</p>
+                <p>MariMasak tidak hanya membantu mencari dan membeli bahan makanan, tetapi juga menyediakan resep dan petunjuk memasak untuk pengguna.</p>
+            </div>
 
-        <div class="link-image">
-        <img src="img/ehem.png" alt="Our Team"></img>
-        </div>
-        </>
-    )
+            <div class="column">
+                <img src="img/ehem.png" alt="Our Team"></img>
+            </div>
+            </>
+            )
 
 }
 
-export default AboutUsPage;
+            export default AboutUsPage;
