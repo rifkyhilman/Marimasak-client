d253acda8daef399b1c33e157fbb5d81ee404a91
diohrtwn <diohartawanarnas@gmail.com>
2023-06-15 16:47:16 +0700

    Update About Us

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -1,70 +1,97 @@
 import React from "react";
-import '../Styles/AboutUsPage.scss';
+import "../Styles/AboutUsPage.scss";
+import masakguysImage from "../Assets/img/masakguys.jpeg";
+import dioImage from "../Assets/img/dio.jpeg";
+import arifImage from "../Assets/img/arif.jpeg";
+import rifkyImage from "../Assets/img/rifky.jpeg";
+import indahImage from "../Assets/img/indah.jpeg";
 
 const AboutUsPage = () => {
-    return (
-        <>
-            <div class="about-section">
-                <h1>MariMasak</h1>
-                <p>MariMasak adalah website yang menyajikan resep makanan khas dari pulau Sumatera. </p>
-                <p>Tujuannya adalah memberikan kemudahan bagi pengguna dalam mendapatkan bahan makanan saat mereka tidak punya waktu untuk berbelanja di pasar.</p>
-                <p>MariMasak tidak hanya membantu mencari dan membeli bahan makanan, tetapi juga menyediakan resep dan petunjuk memasak untuk pengguna.</p>
+  const dioGithubUrl = "https://github.com/diohrtwn";
+  const arifGithubUrl = "https://github.com/arifwibiii";
+  const rifkyGithubUrl = "https://github.com/rifkyhilman";
+  const indahGithubUrl = "https://github.com/indaahw";
+
+  return (
+    <div className="about-us-page">
+      <div className="about-section">
+        <div className="about-content">
+          <h1>Tentang MariMasak</h1>
+          <p>
+            MariMasak adalah website yang menyajikan resep makanan khas dari pulau Sumatera.
+          </p>
+          <p>
+            Tujuannya adalah memberikan kemudahan bagi pengguna dalam mendapatkan bahan makanan saat mereka tidak punya waktu untuk berbelanja di pasar.
+          </p>
+          <p>
+            MariMasak tidak hanya membantu mencari dan membeli bahan makanan, tetapi juga menyediakan resep dan petunjuk memasak untuk pengguna.
+          </p>
+        </div>
+        <div className="about-image">
+          <img src={masakguysImage} alt="MariMasak" />
+        </div>
+      </div>
+
+      <h2 className="about-title">Tentang Kami</h2>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <img src={dioImage} alt="DIO" />
+            <div className="container">
+              <h2>Dio Hartawan Arnas</h2>
+              <p className="title">Universitas Esa Unggul</p>
+              <p>Back-End | Developer</p>
+              <p>
+                <a href={dioGithubUrl} className="button">Connect</a>
+              </p>
             </div>
+          </div>
+        </div>
 
-            <h2>Our Team</h2>
-            <div class="row">
-                <div class="column">
-                    <div class="card">
-                        <img src="img/dio.JPEG" alt="DIO"></img>
-                        <div class="container">
-                            <h2>Dio Hartawan</h2>
-                            <p class="title">Universitas Esa Unggul</p>
-                            <p>jane@example.com</p>
-                            <p><button class="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
+        <div className="column">
+          <div className="card">
+            <img src={arifImage} alt="ARIF" />
+            <div className="container">
+              <h2>Arif Wibisono</h2>
+              <p className="title">Universitas Esa Unggul</p>
+              <p>Back-End | Developer</p>
+              <p>
+                <a href={arifGithubUrl} className="button">Connect</a>
+              </p>
+            </div>
+          </div>
+        </div>
 
-                <div class="column">
-                    <div class="card">
-                        <img src="img/arif.jpeg" alt="ARIF"></img>
-                        <div class="container">
-                            <h2>Arif Wibisono</h2>
-                            <p class="title">Universitas Esa Unggul</p>
-                            <p>jane@example.com</p>
-                            <p><button class="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
+        <div className="column">
+          <div className="card">
+            <img src={rifkyImage} alt="RIFKY" />
+            <div className="container">
+              <h2>Rifky Hilman</h2>
+              <p className="title">Universitas Bina Sarana Informatika</p>
+              <p>Front-End | Developer</p>
+              <p>
+                <a href={rifkyGithubUrl} className="button">Connect</a>
+              </p>
+            </div>
+          </div>
+        </div>
 
-                <div class="column">
-                    <div class="card">
-                        <img src="img/rifky.JPEG" alt="RIFKY"></img>
-                        <div class="container">
-                            <h2>Rifky Hilman</h2>
-                            <p class="title">Universitas Bina Sarana Informatika</p>
-                            <p>jane@example.com</p>
-                            <p><button class="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
+        <div className="column">
+          <div className="card">
+            <img src={indahImage} alt="INDAH" />
+            <div className="container">
+              <h2>Indah Warohmah</h2>
+              <p className="title">Universitas Bina Sarana Informatika</p>
+              <p>Front-End | Developer</p>
+              <p>
+                <a href={indahGithubUrl} className="button">Connect</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                <div class="column">
-                    <div class="card">
-                        <img src="img/indah.JPEG" alt="INDAH"></img>
-                        <div class="container">
-                            <h2>Indah Warohmah</h2>
-                            <p class="title">Universitas Bina Sarana Informatika</p>
-                            <p>jane@example.com</p>
-                            <p><button class="button">Contact</button></p>
-                        </div>
-                    </div>
-                </div>
-            </div>    
-            </>
-            )
-
-}
-
-            export default AboutUsPage;
-
+export default AboutUsPage;
