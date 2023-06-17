57326765703ae4e9f5ae8cb1afd7e7863ce4a858
diohrtwn <diohartawanarnas@gmail.com>
2023-06-17 19:21:28 +0700

    Update Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,51 +1,78 @@
-import Logo from "../Assets/Logo.png";
+import React from "react";
+import ESGULLogo from "../Assets/img/ESGUL.png";
+import BSILogo from "../Assets/img/BSI.png";
+import DicodingLogo from "../Assets/img/dicoding.png";
+import KampusMerdekaLogo from "../Assets/img/kampus-merdeka.png";
 import "../Styles/Footer.scss";
-
-// import icon dari mui5
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
       <div className="footer-wrapper__section-one">
-        <div className="footer-wrapper__section-one__logo-container">
-          <img src={Logo} alt="" />
-        </div>
-        <div className="footer-wrapper__section-one__icons">
-          <TwitterIcon />
-          <LinkedInIcon />
-          <YouTubeIcon />
-          <FacebookIcon />
+        <h3>Tentang MariMasak</h3>
+        <div className="footer-description">
+          <p>MariMasak adalah website</p>
+          <p>yang menyajikan resep</p>
+          <p>makanan khas dari pulau</p>
+          <p>Sumatera.</p>
         </div>
       </div>
       <div className="footer-wrapper__section-two">
         <div className="footer-wrapper__section-two__columns">
-               <a href="/">Beranda</a>
-               <a href="about">Tentang</a>
-               <a href="keranjang">Keranjang</a>
-    
+          <h3>Navigasi</h3>
+          <a href="/">Beranda</a>
+          <a href="about">Tentang</a>
+          <a href="keranjang">Keranjang</a>
         </div>
         <div className="footer-wrapper__section-two__columns">
-          <span>KONTAK KAMI</span>
-          <span><LinkedInIcon /> <a href="https://www.linkedin.com/in/dio-hartawan-arnas-b2239a279/"
-        target="_blank"
-        rel="noopener noreferrer" 
-        >Dio</a></span>
-          <span><LinkedInIcon /> <a href="https://www.linkedin.com/in/arif-wibisono-526424279/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Arif</a> </span>
-           <span><LinkedInIcon /> <a href="https://www.linkedin.com/in/muhammad-rifki-hilman-1a3421192/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Rifky</a></span>
-          <span><LinkedInIcon /> <a href="https://www.linkedin.com/in/indah-warohmah-740bb3262/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Indah</a></span>
+          <h3>Kontak Kami</h3>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/dio-hartawan-arnas-b2239a279/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Dio
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/arif-wibisono-526424279/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Arif
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/muhammad-rifki-hilman-1a3421192/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Rifki
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/indah-warohmah-740bb3262/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Indah
+            </a>
+          </span>
+        </div>
+        <div className="footer-wrapper__section-two__columns">
+          <h3>Didukung Oleh</h3>
+          <div className="logo-container">
+            <img src={ESGULLogo} alt="Universitas Esa Unggul" />
+            <img src={BSILogo} alt="Universitas Bina Sarana Informatika" />
+          </div>
+          <div className="logo-container">
+            <img src={DicodingLogo} alt="Dicoding" />
+            <img src={KampusMerdekaLogo} alt="Kampus Merdeka" />
+          </div>
         </div>
       </div>
     </div>
